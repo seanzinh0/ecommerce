d0799dbb93ddef09cc5113c89f705025b719af53
spichay <seanpichay1015@gmail.com>
2024-11-10 14:47:55 -0500

    created product page and load html from json using javascript

---
+++ script.js
@@ -0,0 +1,24 @@
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => updateUI(json.products));
+
+function updateUI(products) {
+    const productGrid = document.querySelector('.product-grid');
+
+    let result = "";
+    products.forEach(product => {
+        const { image, title, price, description } = product;
+
+        result += `
+            <div class="card__product">
+                <img src="${image}" alt="${title}">
+                <div class="card__product-text">
+                    <h1 class="card__product-title">${title}</h1>
+                    <p class="card__product-price">${price}</p>
+                    <p class="card__product-description">${description}</p>
+                </div>
+            </div>
+        `;
+    });
+    productGrid.innerHTML = result;
+}
