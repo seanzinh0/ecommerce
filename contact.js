b76da5813b63588e8547470e8e8910df73711b41
seanzinh0 <seanpichay1015@gmail.com>
2024-11-17 10:52:18 -0500

    comment code

--- contact.js
+++ contact.js
@@ -1,5 +1,8 @@
+//contact form element
 const contactForm = document.querySelector('.contact-form');
+//add on submit event listener to contact form
 contactForm.addEventListener('submit', (e) => {
+    //input elements
     let firstName = document.getElementById("firstname");
     let lastName = document.getElementById("lastname");
     let email = document.getElementById("email");
@@ -7,20 +10,25 @@ contactForm.addEventListener('submit', (e) => {
     let defaultReason = document.getElementById("default-reason");
     let message = document.getElementById("message");
 
+    //remove error class to clear style when new form is open
     firstName.classList.remove("error")
     lastName.classList.remove("error")
     email.classList.remove("error")
     message.classList.remove("error")
     defaultReason.classList.remove("error")
 
+    //tracks if form is valid
     let isValid = true;
 
+    //if firstname is empty isValid is false and adds error style
     if (firstName.value.trim() === ""){
         firstName.classList.add("error")
         isValid = false;
     }else {
+        //remove error style if isValid is true
         firstName.classList.remove("error")
     }
+    //if lastname is empty isValid is false and adds error style
     if (lastName.value.trim() === ""){
         lastName.classList.add("error")
         isValid = false;
@@ -28,24 +36,28 @@ contactForm.addEventListener('submit', (e) => {
         lastName.classList.remove("error")
     }
     const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    //test email against the regex
     if (!emailRegex.test(email.value)) {
         email.classList.add("error")
         isValid = false;
     }else {
         email.classList.remove("error")
     }
+    //if message is empty isValid is false and adds error style
     if (message.value.trim() === ""){
         message.classList.add("error")
         isValid = false;
     }else {
         message.classList.remove("error")
     }
+    //checks to see if reasons is on default and adds error style and add isValid to false
     if (reasons.value === "" || reasons.value === "default") {
         defaultReason.classList.add("error")
         isValid = false;
     }else {
         defaultReason.classList.remove("error")
     }
+    //if isValid is false prevent default
     if (!isValid) {
         e.preventDefault();
     }
